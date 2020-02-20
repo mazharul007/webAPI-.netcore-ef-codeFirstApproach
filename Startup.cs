bbf947d6d7ff5bb188b5bc13bbb2deb178f2dc6a
unknown <mohammad.mazharul.hoque7@gmail.com>
2020-02-20 18:47:22 +0600

    swashbuckle added

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.OpenApi.Models;
 namespace DattingAppAPI
 {
     public class Startup
@@ -32,7 +32,11 @@ namespace DattingAppAPI
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddMvc(option => option.EnableEndpointRouting = false);
-           
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +47,12 @@ namespace DattingAppAPI
                 app.UseDeveloperExceptionPage();
             }
 
-           
+            app.UseSwagger();
             app.UseMvc();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
